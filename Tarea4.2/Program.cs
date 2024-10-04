a02f5fb84e00fde4734b0b1c2c6114c53db4fa9e
Hector Serna <hectorsernagutierrez@gmail.com>
2024-10-04 20:16:50 +0200

    Carga Genero Prueba

--- Tarea4.2/Program.cs
+++ Tarea4.2/Program.cs
@@ -26,6 +26,57 @@ foreach (var guidUsuario in mCommunityApi.GetCommunityInfo().users)
     //KeyValuePair<Guid, Userlite> primerUsuario = mUserApi.GetUsersByIds(new List<Guid>() { guidUsuario }).FirstOrDefault();
     //string nombrePrimerUsuario = primerUsuario.Value.user_short_name;
 }
-//Cambio contra la rama principal
+#region TesauroComunidad
+mCommunityApi.Log.Debug("Inicio de la Carga del tesauro de la comunidad");
+mCommunityApi.Log.Debug("**************************************");
 
-Console.WriteLine("hol");
+// Si hay recursos Categorizados contra alguna categoría del Tesauro, no se puede borrar el TESAURO
+// Eliminamos el vinculo de cualquier recurso con cualquier Categoría
+//BorrarCategoriasDeRecursos("peliculacrudapi");
+//BorrarCategoriasDeRecursos("personacrudapi");
+string xml2 = mCommunityApi.GetThesaurus();
+Console.WriteLine(xml2);
+// Leemos del XML la estrucutra del Tesauro (Categorías) a cargar en la Comunidad
+XmlDocument xmlCategorias = new XmlDocument();
+xmlCategorias.Load($"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Documents\\ESTRUCTURA_CATEGORIAS_COMPLETO_MOD_SIN.xml");
+//mCommunityApi.CreateThesaurus(xmlCategorias.OuterXml);
+
+//Obtenemos el Tesauro de Categorías de la comunidad (XML)
+string xml = mCommunityApi.GetThesaurus();
+
+//Imprimimos por Consola el Tesauro Cargado
+Console.WriteLine(xml);
+
+mCommunityApi.Log.Debug("**************************************");
+mCommunityApi.Log.Debug("Fin de la Carga del tesauro de comunidad (categorías)'");
+
+#endregion TesauroComunidad
+
+#region Carga de géneros (SECUNDARIA)
+//string identificador = Guid.NewGuid().ToString(); //Se pone en el grafo de ontología
+//Genre genero = new(identificador + "IDdistinctorio"); //Se pone en el grafo de búsqueda
+//genero.Schema_name = "NombreGeneroAhIDdistinctorio";
+//mResourceApi.ChangeOntology("generohectors.owl");
+//SecondaryResource generoSR = genero.ToGnossApiResource(mResourceApi, identificador); 
+//mResourceApi.LoadSecondaryResource(generoSR);
+
+string identificador = Guid.NewGuid().ToString(); //Se pone en el grafo de ontología
+Genre genero = new(identificador); //Se pone en el grafo de búsqueda
+genero.Schema_name = "Fantasía";
+mResourceApi.ChangeOntology("generohectors.owl");
+SecondaryResource generoSR = genero.ToGnossApiResource(mResourceApi, $"Genre_{identificador}");
+string mensajeFalloCarga = $"Error en la carga del Género con identificador {identificador} -> Nombre: {genero.Schema_name}";
+try
+{
+    mResourceApi.LoadSecondaryResource(generoSR);
+    if (!generoSR.Uploaded)
+    {
+        mResourceApi.Log.Error(mensajeFalloCarga);
+    }
+}
+catch (Exception)
+{
+    mResourceApi.Log.Error($"Exception -> {mensajeFalloCarga}");
+}
+#endregion Carga de géneros
+
